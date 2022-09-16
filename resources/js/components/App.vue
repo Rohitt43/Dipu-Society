7bcac46e8b719af3d3774643dc330cd011da60f3
Roet <roethapa43@gmail.com>
2022-09-16 20:01:04 +0545

    Initial commit

---
+++ resources/js/components/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <navbar-component></navbar-component>
+    </div>
+
+    
+</template>
+<script>
+import NavBar from './NavBar.vue'
+
+export default {
+   components:{NavBar},
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ resources/js/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <p>hello there</p>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
