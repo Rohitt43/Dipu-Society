92cbfb9d4c2dac1d35ff6fb2c467f27489b9421c
Roet <roethapa43@gmail.com>
2022-09-30 19:39:49 +0545

    ABout us almost complete

--- resources/js/components/AboutUs.vue
+++ resources/js/components/AboutUs.vue
@@ -11,16 +11,209 @@
 
 			<div class="col-lg-8">
 				<div class="about-item-content pl-4">
-					<h2 class="mb-4 content-title"> Welcome to Charite NonProfit Organization</h2>
-					<p class="lead">Charite is the world’s #1 fundraising site forcharitable causes. Raise money for over 1.5 million charities.</p>
+					<h2 class="mb-4 content-title"> Welcome to DIPU Society</h2>
+					<p class="lead">DIPU Society is the dhangadhi's fundraising site for charitable causes.</p>
 					<p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-					<a href="#" class="btn btn-main rounded">Learn More</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<div class="section pt-0">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-lg-8">
+				<div class="content text-center">
+					<h2 class="content-title mb-4">Nationwide partner</h2>
+					<p>Assumenda reiciendis delectus dolore incidunt molestias omnis quo quaerat voluptate, eligendi perspiciatis ipsa laudantium nesciunt officia, odit nemo quidem hic itaque. Fugiat.</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="clients-item-wrap mt-5">
+			<div class="row">
+				<div class="col-lg-3 col-md-6">
+					<a href="#">
+						<img src="../assets/g7.jpg" alt="" class="img-fluid ">
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<section class="section feature pb-0">
+	<div class="container">
+		<div class="row no-gutters">
+			<div class="col-lg-4 col-md-6">
+				<div class="feature-inner">
+					<i class="fa-solid fa-house-chimney"></i>
+					<h4>Founded in 1962</h4>
+					<p>Work Since 1987 with Pride!!</p>
+					<p>Cum voluptas tenetur, voluptatibus mollitia odio sint, alias debitis quasi ut commodo consequat.</p>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6">
+				<div class="feature-inner">
+					<i class="fa-solid fa-globe"></i>
+					<h4>Our History</h4>
+					<p>Mission to make smile the world</p>
+					<p>Cum voluptas tenetur, voluptatibus mollitia odio sint, alias debitis quasi ut commodo consequat.</p>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6">
+				<div class="feature-inner">
+					<i class="fa-solid fa-people-group"></i>
+					<h4>Large Community</h4>
+					<p>Join our community &amp; help others</p>
+					<p>Cum voluptas tenetur, voluptatibus mollitia odio sint, alias debitis quasi ut commodo consequat.</p>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</section>
+
+<section class="section team">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-lg-7 text-center">
+				<div class="section-title">
+					<span class="h6 text-color" style="color: #f89d35;">Our volunteer</span>
+					<h2 class="mt-3 content-title">Expert volunteer to get best service</h2>
+				</div>
+			</div>
+		</div>
+
+		<div class="row justify-content-center">
+			<div class="col-lg-3 col-md-6 col-sm-6">
+				<div class="team-item-wrap mb-5">
+					<div class="team-item position-relative">
+						<img src="../assets/auth-1.png" alt="" class="img-fluid w-100">
+						<div class="team-img-hover">
+							<ul class="team-social list-inline">
+								<li class="list-inline-item">
+									<a href="#" class="facebook"><i class="icofont-facebook" aria-hidden="true"></i></a>
+								</li>
+								<li class="list-inline-item">
+									<a href="#" class="twitter"><i class="icofont-twitter" aria-hidden="true"></i></a>
+								</li>
+								<li class="list-inline-item">
+									<a href="#" class="instagram"><i class="icofont-instagram" aria-hidden="true"></i></a>
+								</li>
+								<li class="list-inline-item">
+									<a href="#" class="linkedin"><i class="icofont-linkedin" aria-hidden="true"></i></a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="team-item-content">
+						<h4 class="mt-3 mb-0 text-capitalize">Justin hammer</h4>
+						<p>Student</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-lg-3 col-md-6 col-sm-6">
+				<div class="team-item-wrap mb-5">
+					<div class="team-item position-relative">
+						<img src="../assets/auth-1.png" alt="" class="img-fluid w-100">
+						<div class="team-img-hover">
+							<ul class="team-social list-inline">
+								<li class="list-inline-item">
+									<a href="#" class="facebook"><i class="icofont-facebook" aria-hidden="true"></i></a>
+								</li>
+								<li class="list-inline-item">
+									<a href="#" class="twitter"><i class="icofont-twitter" aria-hidden="true"></i></a>
+								</li>
+								<li class="list-inline-item">
+									<a href="#" class="instagram"><i class="icofont-instagram" aria-hidden="true"></i></a>
+								</li>
+								<li class="list-inline-item">
+									<a href="#" class="linkedin"><i class="icofont-linkedin" aria-hidden="true"></i></a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="team-item-content">
+						<h4 class="mt-3 mb-0 text-capitalize">Jason roy</h4>
+						<p>Businessman</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-lg-3 col-md-6 col-sm-6">
+				<div class="team-item-wrap mb-5 ">
+					<div class="team-item position-relative">
+						<img src="../assets/auth-1.png" alt="" class="img-fluid w-100">
+						<div class="team-img-hover">
+							<ul class="team-social list-inline">
+								<li class="list-inline-item">
+									<a href="#" class="facebook"><i class="icofont-facebook" aria-hidden="true"></i></a>
+								</li>
+								<li class="list-inline-item">
+									<a href="#" class="twitter"><i class="icofont-twitter" aria-hidden="true"></i></a>
+								</li>
+								<li class="list-inline-item">
+									<a href="#" class="instagram"><i class="icofont-instagram" aria-hidden="true"></i></a>
+								</li>
+								<li class="list-inline-item">
+									<a href="#" class="linkedin"><i class="icofont-linkedin" aria-hidden="true"></i></a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="team-item-content">
+						<h4 class="mt-3 mb-0 text-capitalize">Henry oswald</h4>
+						<p>CO founder</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-lg-3 col-md-6 col-sm-6">
+				<div class="team-item-wrap mb-5">
+					<div class="team-item position-relative">
+						<img src="../assets/auth-1.png" alt="" class="img-fluid w-100">
+						<div class="team-img-hover">
+							<ul class="team-social list-inline">
+								<li class="list-inline-item">
+									<a href="#" class="facebook"><i class="icofont-facebook" aria-hidden="true"></i></a>
+								</li>
+								<li class="list-inline-item">
+									<a href="#" class="twitter"><i class="icofont-twitter" aria-hidden="true"></i></a>
+								</li>
+								<li class="list-inline-item">
+									<a href="#" class="instagram"><i class="icofont-instagram" aria-hidden="true"></i></a>
+								</li>
+								<li class="list-inline-item">
+									<a href="#" class="linkedin"><i class="icofont-linkedin" aria-hidden="true"></i></a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="team-item-content">
+						<h4 class="mt-3 mb-0 text-capitalize">Jason roy</h4>
+						<p>Businessman</p>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 </section>
 
+<div class="cta-block section">
+	<div class="container">
+		<div class="row justify-content-center ">
+			<div class="col-lg-7 col-md-12">
+				<div class="cta-content text-center">
+					<i class="icofont-diamond text-lg text-color"></i>
+					<h2 class="text-white text-lg mb-5 mt-3">We can’t help everyone, but everyone can help someone</h2>
+					<b-button variant="warning" class="rounded text-white">Make a donation</b-button>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 
 
     <Footer />
